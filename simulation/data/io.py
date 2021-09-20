8dee9d8052625157618925783d2897986dcc217e
Michael Dorner <mail@michaeldorner.de>
2021-09-20 15:57:55 +0200

    remove unnecessary code during JSON encoding

--- simulation/data/io.py
+++ simulation/data/io.py
@@ -5,11 +5,6 @@ import orjson
 
 _data_dir = os.path.dirname(os.path.abspath(__file__))
 
-def _default(obj):
-    if isinstance(obj, set):
-        return {o: None for o in obj}
-    raise TypeError
-
 def _create_file_name(is_time_respecting: bool, postfix_name: str):
     file_name = {
         True: 'time_respecting_',
@@ -30,12 +25,10 @@ def load_simulation_parameters() -> dict:
 
 
 def store_horizons(horizons, is_time_respecting: bool, postfix_name: str = 'horizons.json'):
-    if is_time_respecting:
-        horizons = {str(k): {str(kk): horizons[k][kk].isoformat(
-        ) for kk in horizons[k]} for k in horizons}
-    else:
-        horizons = {str(k): [str(kk) for kk in horizons[k]] for k in horizons}
-    print('start jsonizing...')
+    def _default(obj):
+        if isinstance(obj, set):
+            return {o: None for o in obj}
+        raise TypeError
     b = orjson.dumps(horizons, default=_default)
     with open(_create_file_name(is_time_respecting, postfix_name), 'wb') as f:
         f.write(b)
