ae1e092f66c5bb67c9d7b74b42add7b79f921aa5
Michael Dorner <mail@michaeldorner.de>
2021-09-23 15:57:19 +0200

    fix --skip_storing_horizon

--- simulation/__main__.py
+++ simulation/__main__.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
                         help='Simulate the time-ignoring model only.')
     parser.add_argument('--time_respecting_only', action='store_true',
                         help='Simulate the time-respecting model only.')
-    parser.add_argument('--skip_storing_horizon', action='store_true',
+    parser.add_argument('--skip_storing_horizon', action='store_false',
                         help='Do not store the horizons as JSON (~50 GB needed) before calculating the cardinalities.')
     args = parser.parse_args()
 
@@ -19,5 +19,4 @@ if __name__ == '__main__':
         consider_time = [False]
     elif not args.time_ignoring_only and args.time_respecting_only:
         consider_time = [True]
-
     simulation.run(consider_time, args.skip_storing_horizon)
