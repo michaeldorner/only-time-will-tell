19803c4dfcfb74554f3473a0dd48066ec8b2eaa3
Michael Dorner <mail@michaeldorner.de>
2021-11-14 12:55:31 +0100

    fix path

--- test/test_model.py
+++ test/test_model.py
@@ -4,7 +4,7 @@ from simulation import model, io
 
 class ModelTest(unittest.TestCase):
     def test_model(self):
-        data = io.load_json('./data/simulation_parameters.json')
+        data = io.load_json('../data/simulation_parameters.json')
         cn = model.CommunicationNetwork(data)
         self.assertEqual(len(cn.participants), 37103)
         self.assertEqual(len(cn.channels), 309740)
