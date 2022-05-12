e4a0a935e7c6a482bf8a780fdaad060d487756e1
Michael Dorner <mail@michaeldorner.de>
2022-05-12 13:27:50 +0200

    explicit hedge neighbor search for a better performance

--- simulation/traversal.py
+++ simulation/traversal.py
@@ -1,20 +1,26 @@
-from collections import deque
 from .model import Hypergraph
 
 
 def bfs(hypergraph: Hypergraph, source_vertex, distance_attr=None, filter_distance=lambda distance: False):
-    d = deque()
+    backlog = set()
     for source_hedge in hypergraph.hyperedges(source_vertex):
-        d.append(source_hedge)
+        backlog.add(source_hedge)
 
     visited_hedges = set()
     seen_vertices = set()
-    while d:
-        hedge = d.pop()
-        seen_vertices |= hypergraph.vertices(hedge)
-        for next_hedge in hypergraph.hyperedge_neighbors(hedge, distance_attr, filter_distance):
-            if next_hedge not in visited_hedges:
-                visited_hedges.add(next_hedge)
-                d.append(next_hedge)
 
+    while backlog:
+        hedge = backlog.pop()
+        for vertex in hypergraph.vertices(hedge):
+            if vertex not in seen_vertices:
+                seen_vertices.add(vertex)
+                for next_hedge in hypergraph.hyperedges(vertex):
+                    if distance_attr:
+                        distance = hypergraph.get_hedge_data(distance_attr, next_hedge) - \
+                            hypergraph.get_hedge_data(distance_attr, hedge)
+                    else:
+                        distance = None
+                    if next_hedge not in visited_hedges and not filter_distance(distance):
+                        visited_hedges.add(next_hedge)
+                        backlog.add(next_hedge)
     return seen_vertices
