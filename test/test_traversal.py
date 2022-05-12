8523a61a67d1a7fcc9b17ba0e8d07c579f81dd19
Michael Dorner <mail@michaeldorner.de>
2022-05-12 13:26:51 +0200

    Add networkx's BFS comparison

--- test/test_traversal.py
+++ test/test_traversal.py
@@ -1,4 +1,8 @@
 import unittest
+import random
+
+import networkx as nx
+
 from simulation.model import Hypergraph
 from simulation.traversal import bfs
 
@@ -41,3 +45,11 @@ class TraversalTest(unittest.TestCase):
         with self.subTest():
             tr_bfs = bfs(self.h, 'v4', 't', lambda d: d <= 0)
             self.assertEqual(tr_bfs, {'v3', 'v4'})
+
+    def test_bfs_networkx(self):
+
+        bipartite = self.h._bipartite_graph
+        for source_node in random.choices(list(self.h.vertices()), k=10):
+            r_networkx = {n for n in nx.node_connected_component(bipartite, source_node) if n in self.h.vertices()}
+            r_bfs = bfs(self.h, source_node)
+            self.assertEqual(r_networkx, r_bfs)
